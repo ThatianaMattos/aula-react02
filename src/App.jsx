5a4cb9d68f8b811676b31c14aeb2f9af0c638a15
ThatianaMattos <thatianamattos25@gmail.com>
2025-12-11 01:04:55 -0300

    Aula React 02 concluída

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/header/header";
+import Main from "./components/main/main";
+import "./globalStyle.css";
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
+
+export default App;
